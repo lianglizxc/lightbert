3438b3cccf55a2f0f4f44cf953de74ac708143c0
lianglizxc <lianglizxc@163.com>
2022-10-21 18:02:13 +0800

    liangli

--- text_summary.py
+++ text_summary.py
@@ -1,2 +1,9 @@
-from gensim.summarization import keywords
-from gensim.summarization.summarizer import summarize
+from transformers import BertTokenizer, BartForConditionalGeneration, SummarizationPipeline
+import pandas as pd
+tokenizer = BertTokenizer.from_pretrained("fnlp/bart-base-chinese")
+model = BartForConditionalGeneration.from_pretrained("fnlp/bart-base-chinese")
+summary_generator = SummarizationPipeline(model, tokenizer)
+
+finance_data = pd.read_excel('finance_data/SmoothNLP专栏资讯数据集样本10k.xlsx')
+summary = summary_generator(finance_data['content'].iloc[0][:512], max_length=512, do_sample=False)
+print(summary)
