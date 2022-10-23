f18ec8a5ad11b0651d95f93b4925aa0615b18fed
unknown <lianglizxc>
2022-10-23 23:34:09 +0800

    liangli

--- text_summary.py
+++ text_summary.py
@@ -1,9 +1,15 @@
 from transformers import BertTokenizer, BartForConditionalGeneration, SummarizationPipeline
 import pandas as pd
-tokenizer = BertTokenizer.from_pretrained("fnlp/bart-base-chinese")
-model = BartForConditionalGeneration.from_pretrained("fnlp/bart-base-chinese")
-summary_generator = SummarizationPipeline(model, tokenizer)
 
-finance_data = pd.read_excel('finance_data/SmoothNLP专栏资讯数据集样本10k.xlsx')
-summary = summary_generator(finance_data['content'].iloc[0][:512], max_length=512, do_sample=False)
-print(summary)
+def get_sample_summary():
+    tokenizer = BertTokenizer.from_pretrained("fnlp/bart-base-chinese")
+    model = BartForConditionalGeneration.from_pretrained("fnlp/bart-base-chinese")
+    summary_generator = SummarizationPipeline(model, tokenizer)
+
+    finance_data = pd.read_excel('finance_data/SmoothNLP专栏资讯数据集样本10k.xlsx')
+    summary = summary_generator(finance_data['content'].iloc[0][:512], max_length=512, do_sample=False)
+    print(summary)
+
+
+if __name__ == '__main__':
+    get_sample_summary()
